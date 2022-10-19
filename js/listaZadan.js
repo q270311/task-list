0a107f59764e94bb5bf82457ce058ee97d3f2d04
Michał Bałdyga <q270311@gmail.com>
2022-10-19 22:20:55 +0200

    render task list function added

--- js/listaZadan.js
+++ js/listaZadan.js
@@ -1,12 +1,37 @@
 {
+    const taskTable = [
+        {
+            task: "zadanie 1",
+            status: true
+        },
+        {
+            task: "zadanie 2",
+            status: true
+        }];
+
+    const render=()=>{
+        let textRender="";
+        const listTaskElement=document.querySelector(".js-taskList");
+
+        taskTable.forEach(element => {
+            textRender+=`<li class="list__item">
+                    <img src="img/green-icon.png" alt="green icon">
+                    <span>${element.task}</span>
+                    <img src="img/trash-can.jpg" alt="trash can icon">  
+            </li>`;            
+        });
+        listTaskElement.innerHTML=textRender;
+    }
+
     const addNewTaskToList = () => {
         const taskTextElement = document.querySelector(".js-taskText");
         taskTextElement.focus();
+        render();
     }
     const init = () => {
         const addTaskButton = document.querySelector(".js-addTaskButton");
-
         addTaskButton.addEventListener("click", addNewTaskToList);
+        render();
     }
     init();
 }
