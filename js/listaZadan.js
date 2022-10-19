431ad403eed1a4cafdfd85deda695c3c88e3934b
Michał Bałdyga <q270311@gmail.com>
2022-10-19 21:39:20 +0200

    focus to text task element add

--- js/listaZadan.js
+++ js/listaZadan.js
@@ -1,6 +1,12 @@
 {
-    
+    const addNewTaskToList = () => {
+        const taskTextElement = document.querySelector(".js-taskText");
+        taskTextElement.focus();
+    }
     const init = () => {
+        const addTaskButton = document.querySelector(".js-addTaskButton");
+
+        addTaskButton.addEventListener("click", addNewTaskToList);
     }
     init();
 }
