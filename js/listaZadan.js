3d59b34b4da239de63de8a297cce702415e0c8a0
Michał Bałdyga <q270311@gmail.com>
2022-10-20 22:07:27 +0200

    adding nwe task

--- js/listaZadan.js
+++ js/listaZadan.js
@@ -1,47 +1,59 @@
 {
-    const taskTable = [
-        {
-            content: "zadanie 1",
-            done: true
-        },
-        {
-            content: "zadanie 2",
-            done: false
-        }];
+   const taskTable = [
+      {
+         content: "zadanie 1",
+         done: true
+      },
+      {
+         content: "zadanie 2",
+         done: false
+      }];
 
-    const render=()=>{
-        let htmlString="";
+   const render = () => {
+      let htmlString = "";
 
-        taskTable.forEach(element => {
-            if(element.done){
-                htmlString+=`<li class="list__item list__item--lineThrough">
-                    <img src="img/accept-icon.png" alt="green icon">`; 
-            }else{
-                htmlString+=`<li class="list__item">
+      taskTable.forEach(element => {
+         if (element.done) {
+            htmlString += `<li class="list__item list__item--lineThrough">
+                    <img src="img/accept-icon.png" alt="green icon">`;
+         } else {
+            htmlString += `<li class="list__item">
                     <img src="img/green-icon.png" alt="green icon">`;
-            }
-            htmlString+=`<span>${element.content}</span>
+         }
+         htmlString += `<span>${element.content}</span>
                 <img src="img/trash-can.jpg" alt="trash can icon">  
-            </li>`; 
-        });
-        document.querySelector(".js-taskList").innerHTML=htmlString;
-    }
-    
-    const init = () => {
-        const taskTextElement = document.querySelector(".js-taskText");
+            </li>`;
+      });
+      document.querySelector(".js-taskList").innerHTML = htmlString;
+   }
+   const addNewTask=(newTaskContent)=>{
+      taskTable.push({
+         content:newTaskContent,
+      });
+      render();
+   }
 
-        const form=document.querySelector(".js-form");
-        form.addEventListener("submit",(event)=>{
-            event.preventDefault();
-            
-            render();
-        });
+   const onFormSubmit= (event) => {
+      event.preventDefault();
+      const taskTextElement = document.querySelector(".js-taskText");
+      const newTaskContent = taskTextElement.value.trim();
+      if (newTaskContent === "") {
+         return;
+      }
+      addNewTask(newTaskContent);
+      taskTextElement.value="";
+   }
 
-        const addTaskButton=document.querySelector(".js-addTaskButton");
-        addTaskButton.addEventListener("click",()=>{         
-            taskTextElement.focus();
-        });
-        render();
-    }
-    init();
+   const init = () => { 
+
+      const form = document.querySelector(".js-form");
+      form.addEventListener("submit",onFormSubmit);
+
+      const addTaskButton = document.querySelector(".js-addTaskButton");
+      addTaskButton.addEventListener("click", () => {
+         document.querySelector(".js-taskText").focus();
+      });
+      render();
+   }
+   init();
 }
