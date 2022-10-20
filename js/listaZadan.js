1cd802e1ec5fea4f1c988609e2f851b11da2ab5b
Michał Bałdyga <q270311@gmail.com>
2022-10-20 21:37:36 +0200

    form submit added

--- js/listaZadan.js
+++ js/listaZadan.js
@@ -1,36 +1,46 @@
 {
     const taskTable = [
         {
-            task: "zadanie 1",
-            status: true
+            content: "zadanie 1",
+            done: true
         },
         {
-            task: "zadanie 2",
-            status: true
+            content: "zadanie 2",
+            done: false
         }];
 
     const render=()=>{
-        let textRender="";
-        const listTaskElement=document.querySelector(".js-taskList");
+        let htmlString="";
 
         taskTable.forEach(element => {
-            textRender+=`<li class="list__item">
-                    <img src="img/green-icon.png" alt="green icon">
-                    <span>${element.task}</span>
-                    <img src="img/trash-can.jpg" alt="trash can icon">  
-            </li>`;            
+            if(element.done){
+                htmlString+=`<li class="list__item list__item--lineThrough">
+                    <img src="img/accept-icon.png" alt="green icon">`; 
+            }else{
+                htmlString+=`<li class="list__item">
+                    <img src="img/green-icon.png" alt="green icon">`;
+            }
+            htmlString+=`<span>${element.content}</span>
+                <img src="img/trash-can.jpg" alt="trash can icon">  
+            </li>`; 
         });
-        listTaskElement.innerHTML=textRender;
+        document.querySelector(".js-taskList").innerHTML=htmlString;
     }
-
-    const addNewTaskToList = () => {
-        const taskTextElement = document.querySelector(".js-taskText");
-        taskTextElement.focus();
-        render();
-    }
+    
     const init = () => {
-        const addTaskButton = document.querySelector(".js-addTaskButton");
-        addTaskButton.addEventListener("click", addNewTaskToList);
+        const taskTextElement = document.querySelector(".js-taskText");
+
+        const form=document.querySelector(".js-form");
+        form.addEventListener("submit",(event)=>{
+            event.preventDefault();
+            
+            render();
+        });
+
+        const addTaskButton=document.querySelector(".js-addTaskButton");
+        addTaskButton.addEventListener("click",()=>{         
+            taskTextElement.focus();
+        });
         render();
     }
     init();
